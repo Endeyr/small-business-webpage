e6a6e0419666c4dd5f14deb6b31b60e0fc3182d4
Endeyr <aaron.thompson42594@gmail.com>
2024-02-06 13:47:51 -0500

    add ui components

---
+++ app/ui/input/index.tsx
@@ -0,0 +1,32 @@
+import { twMerge } from 'tailwind-merge'
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+	label?: string
+	description?: string
+}
+
+const Input: React.FC<InputProps> = ({
+	label,
+	description,
+	className,
+	...props
+}) => {
+	const baseClasses: string =
+		'block w-full rounded-md border-gray-200 px-4 py-4 text-sm focus:border-emerald-500 focus:ring-emerald-500 disabled:pointer-events-none disabled:opacity-50'
+
+	const mergedClasses: string = twMerge([baseClasses, className])
+	// requires npm i -D @tailwindcss/forms
+	// add formsPlugin import to tailwind.config.js
+	return (
+		<div className="max-w-sm space-y-3">
+			{label && (
+				<label className="mb-2 block text-sm font-medium">{label}</label>
+			)}
+			<input {...props} className={mergedClasses} />
+			{description && (
+				<p className="mt-2 text-sm text-gray-500">{description}</p>
+			)}
+		</div>
+	)
+}
+export default Input
