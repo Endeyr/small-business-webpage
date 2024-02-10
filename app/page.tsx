0479b5ae7f106337a240bb2b379a8508018f1fd3
Endeyr <aaron.thompson42594@gmail.com>
2024-02-10 10:50:05 -0500

    removed un-needed import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import CallToAction from './components/call-to-action'
 import Hero from './components/hero'
 import Identity from './components/identity'
-import Layout from './components/layout'
 import Services from './components/services'
 import Testimonials from './components/testimonials'
 
