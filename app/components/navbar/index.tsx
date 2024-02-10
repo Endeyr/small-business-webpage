87ffe771938e2b33f645976e4d7c305b331b471a
Endeyr <aaron.thompson42594@gmail.com>
2024-02-10 10:50:26 -0500

    remove un-needed classes

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -7,12 +7,12 @@ const Navbar = ({ logo }: { logo: string }) => {
 	return (
 		<>
 			{/* Navbar */}
-			<header className="sticky top-0 z-50 w-full border-b bg-white dark:bg-black hidden sm:block">
+			<header className="sticky top-0 z-50 w-full border-b bg-white dark:bg-black hidden md:block">
 				<div className="flex h-16 items-center p-[50px]">
 					<div className="mx-auto w-full space-y-20">
 						<div className="flex justify-between">
 							{/* Left Side Container */}
-							<div className="flex flex-1 items-center justify-start gap-4">
+							<div className="flex items-center justify-start gap-4">
 								<Link
 									href="/"
 									className="inline-flex h-10 items-center justify-center text-lg font-bold text-gray-800 dark:text-white hover:text-[#4078c8]"
@@ -21,7 +21,7 @@ const Navbar = ({ logo }: { logo: string }) => {
 								</Link>
 							</div>
 							{/* Right Side Container */}
-							<div className="flex flex-1 items-center justify-end">
+							<div className="flex items-center justify-end">
 								<nav className="flex items-center space-x-1 gap-4">
 									<Link
 										href="/services"
@@ -61,7 +61,6 @@ const Navbar = ({ logo }: { logo: string }) => {
 					</div>
 				</div>
 			</header>
-			{/* Mobile Navbar */}
 		</>
 	)
 }
