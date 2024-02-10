a492e8b899b4fff23b22c091ef0f67d65e4a1d32
Endeyr <aaron.thompson42594@gmail.com>
2024-02-10 10:50:16 -0500

    add mobile nav

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -1,11 +1,12 @@
 import Footer from '../footer'
 import Navbar from '../navbar'
-
+import MobileNav from '../navbar/mobile-nav'
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
 			<div className="relative min-h-screen bg-white dark:bg-black">
 				<Navbar logo="logo" />
+				<MobileNav logo="logo" />
 				{/* Main Content Area */}
 				<main className="bg-gray-100">
 					<div className="mx-auto">{children}</div>
