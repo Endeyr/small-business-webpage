ae60b6659969051d743e2bc3c903e2cf376e3193
Endeyr <aaron.thompson42594@gmail.com>
2024-02-12 09:46:52 -0500

    add services and identity to home page

--- app/components/identity/index.tsx
+++ app/components/identity/index.tsx
@@ -1,6 +1,41 @@
+import IdentityImg from '@/public/images/services/wolf-sanctuary.webp'
+import Image from 'next/image'
 import Container from '../container'
 
 const Identity = () => {
-	return <Container className="bg-white">Who You Are</Container>
+	return (
+		<Container className="bg-white flex-col justify-center gap-2">
+			<div className="grid grid-col-3 w-full">
+				<Image
+					src={IdentityImg.src}
+					alt="Five wolves running in the woods"
+					width={700}
+					height={350}
+				/>
+				<div className="col-span-2 col-start-2 flex flex-col justify-start items-center gap-2">
+					<h2 className="font-bold capitalize text-lg text-center w-full my-2">
+						Who are we?
+					</h2>
+					<p className="px-4">
+						We are Apollo&apos;s Rescue, a sanctuary nestled atop Mount Olympus,
+						dedicated to providing refuge, rehabilitation, and love to wolves in
+						need. Our mission is to empower these majestic creatures through
+						compassion and sanctuary, preserving their spirit for generations to
+						come.
+					</p>
+					<p className="px-4">
+						At Apollo&apos;s Rescue, we are more than just a sanctuary; we are a
+						beacon of hope, a haven for wolves seeking solace, and a voice for
+						their protection and conservation. Join us in our mission to make a
+						difference in the lives of these incredible animals.
+					</p>
+					<p className="px-4">
+						We are Apollo&apos;s Rescue, where compassion meets purpose, and
+						every howl signifies a story of resilience and hope.
+					</p>
+				</div>
+			</div>
+		</Container>
+	)
 }
 export default Identity
--- app/components/services/index.tsx
+++ app/components/services/index.tsx
@@ -1,9 +1,29 @@
-import Container from "../container";
+import { ContentCard } from '@/app/ui/card'
+import Container from '../container'
 
 const Services = () => {
 	return (
-		<Container className="bg-gray-100">
-			3 Key Services / Products ie What You Do
+		<Container className="bg-gray-100 flex flex-col w-full h-screen justify-evenly items-center">
+			<h2 className="font-bold outline-black capitalize text-4xl text-center w-full">
+				Services
+			</h2>
+			<div className="flex w-full justify-between items-start">
+				<ContentCard title="Sanctuary and Rehabilitation">
+					Providing a safe and nurturing environment for wolves rescued from
+					various situations such as abuse, neglect, or displacement, offering
+					rehabilitation services to help them heal physically and emotionally.
+				</ContentCard>
+				<ContentCard title="Medical Care">
+					Offering comprehensive veterinary care to ensure the health and
+					well-being of the wolves, including routine check-ups, vaccinations,
+					and treatment for injuries or illnesses.
+				</ContentCard>
+				<ContentCard title="Educational Programs">
+					Conducting educational tours, workshops, and outreach programs to
+					raise awareness about wolf conservation, habitat preservation, and
+					responsible stewardship of wildlife.
+				</ContentCard>
+			</div>
 		</Container>
 	)
 }
