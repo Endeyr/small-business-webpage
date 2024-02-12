c58ee0f4686a976dc2176f734662a28604e562ff
Endeyr <aaron.thompson42594@gmail.com>
2024-02-12 09:58:07 -0500

    add testimonials and call to action form to homepage

--- app/components/call-to-action/index.tsx
+++ app/components/call-to-action/index.tsx
@@ -1,11 +1,61 @@
+import Button from '@/app/ui/button'
 import Input from '@/app/ui/input'
 import Container from '../container'
 
 const CallToAction = () => {
 	return (
-		<Container className="bg-white">
-			Call to Action form / login / register
-			<Input />
+		<Container className="bg-white flex-col">
+			<h2 className="font-bold outline-black capitalize text-4xl text-center w-full">
+				Join us in making a difference!
+			</h2>
+			<p>
+				Whether you&apos;re passionate about wildlife conservation, seeking a
+				unique volunteer opportunity, or simply want to support our cause,
+				there&apos;s a place for you at Apollo&apos;s Rescue. Take action today
+				and be a part of our mission to provide sanctuary, rehabilitation, and
+				love to wolves in need. Together, we can make a lasting impact on the
+				lives of these majestic creatures. Join Apollo&apos;s Rescue and be a
+				voice for the voiceless.
+			</p>
+			<form className="grid grid-cols-2 justify-center items-center gap-2">
+				<Input
+					type="text"
+					name="first name"
+					label="First name"
+					placeholder="John"
+				/>
+				<Input
+					type="text"
+					name="last name"
+					label="Last name"
+					placeholder="Smith"
+				/>
+				<Input
+					type="tel"
+					name="phone"
+					label="Phone number"
+					placeholder="555-555-5555"
+				/>
+				<Input
+					type="email"
+					name="email"
+					label="Email"
+					placeholder="johnsmith@email.com"
+				/>
+				<div className="w-full space-y-3 col-span-2 mb-2">
+					<label htmlFor="message" className="mb-2 block text-sm font-medium">
+						Message
+					</label>
+					<textarea
+						rows={4}
+						cols={100}
+						className="block w-full rounded-md border-gray-200 px-4 py-4 text-sm focus:border-[#4078c8] focus:ring-[#4078c8] disabled:pointer-events-none disabled:opacity-50"
+						name="message"
+						placeholder="I am reaching out to volunteer. These are my available times."
+					></textarea>
+				</div>
+				<Button className="col-start-2">Submit</Button>
+			</form>
 		</Container>
 	)
 }
--- app/components/testimonials/index.tsx
+++ app/components/testimonials/index.tsx
@@ -1,9 +1,45 @@
+import { ContentCard } from '@/app/ui/card'
 import Container from '../container'
 
 const Testimonials = () => {
 	return (
-		<Container className="bg-gray-100">
-			Testimonials / Reviews / Case Studies, 1 to 4 different ones
+		<Container className="bg-gray-100 flex flex-col w-full justify-evenly items-center">
+			<h2 className="font-bold outline-black capitalize text-4xl text-center w-full">
+				Testimonials
+			</h2>
+			<div className="flex w-full justify-between items-start">
+				<ContentCard title="5 out of 5 stars!" footer="- Zeus">
+					&quot;Visiting Apollo&apos;s Rescue was an unforgettable experience!
+					The dedication of the staff to the well-being of the wolves is truly
+					inspiring. It&apos;s heartwarming to see these majestic creatures
+					thriving in such a loving environment. A must-visit for anyone
+					passionate about wildlife conservation.&quot;
+				</ContentCard>
+				<ContentCard title="Saved my wolf's life!" footer="- Poseidon">
+					&quot;I had the pleasure of volunteering at Apollo&apos;s Rescue, and
+					it was an incredibly rewarding experience. The team&apos;s commitment
+					to providing top-notch care for the wolves is unmatched. I witnessed
+					firsthand the positive impact of their rehabilitation programs.
+					Apollo&apos;s Rescue is making a real difference in the lives of these
+					animals.&quot;
+				</ContentCard>
+				<ContentCard title="Great Experience!" footer="- Ares">
+					&quot;Apollo&apos;s Rescue is a true gem in the world of animal
+					sanctuaries. The staff&apos;s expertise and genuine love for the
+					wolves shine through in every aspect of their work. The sanctuary
+					itself is beautifully maintained, providing a safe and enriching
+					environment for its residents. I left feeling inspired and grateful
+					for the opportunity to support such a worthy cause.&quot;
+				</ContentCard>
+				<ContentCard title="Fun and Educational!" footer="- Percy">
+					&quot;Words cannot express how grateful I am for Apollo&apos;s Rescue.
+					After visiting the sanctuary, I gained a newfound appreciation for
+					wolves and their importance in the ecosystem. The dedication of the
+					team to their well-being is evident in every interaction.
+					Apollo&apos;s Rescue is a beacon of hope for these incredible animals,
+					and I&apos;m proud to support their mission.&quot;
+				</ContentCard>
+			</div>
 		</Container>
 	)
 }
